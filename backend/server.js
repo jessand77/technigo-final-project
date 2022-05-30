edf70e522dc0c3fdb308db1183943d10e1dc60dc
Jessica Sandler <jessica.sandler@hotmail.com>
2022-05-30 16:45:32 +0200

    connect to db

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,37 @@ import mongoose from "mongoose";
 
 import marathons from "./data/marathons.json"
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalProjectApi";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+const { Schema } = mongoose
+
+const marathonSchema = new Schema({
+  name: String,
+  city: String,
+  country: String,
+  website: String
+})
+
+const Marathon = mongoose.model("Marathon", marathonSchema)
+
 const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
+
 
 // Start defining your routes here
 app.get("/", (req, res) => {
@@ -20,6 +45,18 @@ app.get('/marathons', (req, res) => {
   res.status(200).json({
     marathons: marathons,
   })
+})
+
+app.get('/allmarathons', async (req, res) => {
+  try {
+    const allmarathons = await Marathon.find()
+      .exec()
+    res.status(200).json(allmarathons)
+  } catch (err) {
+    res.status(400).json({
+      error: err.errors,
+    })
+  }
 })
 
 // Start the server
