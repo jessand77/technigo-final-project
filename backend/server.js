18bcc0c38339a35740307ad918cd8915d4c90116
Jessica Sandler <jessica.sandler@hotmail.com>
2022-06-03 11:03:59 +0200

    Add dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from "mongoose";
+/* eslint-disable no-undef */
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import listEndpoints from 'express-list-endpoints';
 
-// Delete this later when the data from MongoDB
-import marathons from "./data/marathons.json"
+// Delete this later when the data from MongoDB works?
+import marathons from './data/marathons.json';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalProjectApi";
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/finalProjectApi';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
 const marathonSchema = new Schema({
   name: String,
   city: String,
   country: String,
-  website: String
-})
+  website: String,
+});
 
-const Marathon = mongoose.model("Marathon", marathonSchema)
+const Marathon = mongoose.model('Marathon', marathonSchema);
+
+dotenv.config();
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -29,36 +34,39 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
-    next()
+    next();
   } else {
-    res.status(503).json({ error: 'Service unavailable' })
+    res.status(503).json({ error: 'Service unavailable' });
   }
-})
+});
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+app.get('/secret', (req, res) => {
+  res.send(process.env.SECRET_KEY);
 });
 
 // Gets the races from internal json file
 app.get('/marathons', (req, res) => {
   res.status(200).json({
     marathons: marathons,
-  })
-})
+  });
+});
 
-// Gets the races from MongoDB 
+// Gets the races from MongoDB
 app.get('/allmarathons', async (req, res) => {
   try {
-    const allmarathons = await Marathon.find()
-      .exec()
-    res.status(200).json(allmarathons)
+    const allmarathons = await Marathon.find().exec();
+    res.status(200).json(allmarathons);
   } catch (err) {
     res.status(400).json({
       error: err.errors,
-    })
+    });
   }
-})
+});
 
 // Start the server
 app.listen(port, () => {
