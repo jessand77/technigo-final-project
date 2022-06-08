dc83b9f6795e6a0515f1857cc4b7cf6b2109b7bd
Jessica Sandler <jessica.sandler@hotmail.com>
2022-06-08 15:10:19 +0200

    Change Start component

--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 import MarathonList from './MarathonList';
 
 const Start = () => {
-    return (
-        <div>
-            <h1>Start</h1>
-            <button><Link to="/login">Login</Link></button>
-            <button><Link to="/register">Register</Link></button>
-            <MarathonList />
-        </div>
-    );
+	return (
+		<div>
+			<h1>Start</h1>
+			<h2>En till rad</h2>
+			<button>
+				<Link to="/login">Login</Link>
+			</button>
+			<button>
+				<Link to="/register">Register</Link>
+			</button>
+			<MarathonList />
+		</div>
+	);
 };
 
 export default Start;
