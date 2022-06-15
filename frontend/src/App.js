5ef7026af55b66584044abc8f305c67f92d98672
Jessica Sandler <jessica.sandler@hotmail.com>
2022-06-15 13:38:07 +0200

    Add OuterWrapper and InnerWrapper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import AppContainer from 'AppContainer';
+import styled from 'styled-components';
 
+import StartPage from './pages/StartPage';
+import LoginOrRegister from 'pages/LoginOrRegister';
+import UserPage from './pages/UserPage';
+import EndPage from 'pages/EndPage';
+import NotFound from './pages/NotFound';
 
 import user from 'reducers/user';
 import ui from 'reducers/ui';
@@ -14,10 +20,34 @@ const reducer = combineReducers({
 
 const store = configureStore({ reducer });
 
+const OuterWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const InnerWrapper = styled.div`
+	width: 70%;
+	margin: 0 auto;
+`;
+
 export const App = () => {
 	return (
-			<Provider store={store}>
-				<AppContainer />
-			</Provider>
+		<OuterWrapper>
+			<InnerWrapper>
+				<Provider store={store}>
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" element={<StartPage />}></Route>
+							<Route path="/login" element={<LoginOrRegister />}></Route>
+							<Route path="/userpage" element={<UserPage />}></Route>
+							<Route path="/loggedout" element={<EndPage />}></Route>
+							<Route path="*" element={<NotFound />}></Route>
+						</Routes>
+					</BrowserRouter>
+				</Provider>
+			</InnerWrapper>
+		</OuterWrapper>
 	);
 };
