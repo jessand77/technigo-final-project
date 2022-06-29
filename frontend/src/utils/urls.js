957f21656efc49c7ed798d19194e9ff41a712bc2
Jessica Sandler <jessica.sandler@hotmail.com>
2022-06-29 22:11:04 +0200

    Change BASE_URL

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://technigo-final-project.herokuapp.com/';
-const BASE_URL = 'http://localhost:8080/';
+export const BASE_URL = 'https://technigo-final-project.herokuapp.com/';
+// const BASE_URL = 'http://localhost:8080/';
 
 export const API_URL = (slug) => `${BASE_URL}${slug}`;
 export const API_KEY = '90138b7b14dd18ad14d7fe7a74d94a61';
